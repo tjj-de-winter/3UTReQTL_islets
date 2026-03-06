fbf32deeda93d9da2e7d4c0fc2a40cc95aebb817
tjj-de-winter <140805829+tjj-de-winter@users.noreply.github.com>
2026-03-06 10:08:51 +0100

    Update fastqdump_geo.sh

--- bin/data_download/fastqdump_geo.sh
+++ bin/data_download/fastqdump_geo.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# generate fastq files from the downloaded .sra files from patch-seq datasets processed with smartseq2
+sra_txt=$1 # SRR_Acc_list.txt file
+outdir=$2
+sra_dir=$3
 
-sra_dir=/exports/ana-scarlab/tdwinter/SRA_downloads/sra
-
-Accesion=( GSE83139, GSE81547, GSE81608, GSE86469, GSE73727, GSE124742, GSE164875, GSE270484 )
-for ID in "${Accesion[@]}"
+for srr in $(awk '{print $0}' $sra_txt);
 do
-	echo "$ID"
-	outdir=${ID}_fastq
-	mkdir -p $outdir
-
-	sbatch --export=All -J ${ID#GSE} -e ${ID}.err -o ${ID}.out -t 7-24:00:00 --mem=30G --wrap="./sra2fastq.sh SRR_Acc_List_${ID}.txt $outdir $sra_dir"
+	fastq-dump --outdir $outdir --split-files --gzip ${sra_dir}/${srr}.sra;
 done
 
 
